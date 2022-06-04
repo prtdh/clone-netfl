0bd236c0dd2b2b867be1829a17a753ee33c9c687
Preet <prtdh20@gmail.com>
2022-06-04 20:56:34 +0530

    background image set

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
+import Homepage from "./components/Home/Homepage";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
+    < >
+    <Homepage/>
+    </>
   );
 }
 
---
+++ src/components/Home/Homepage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./Homepage.css"
+const Homepage = () => {
+  return (
+    <>
+    <div className="container">
+      <img className='background-image' src="https://assets.nflxext.com/ffe/siteui/vlv3/5ea364b1-8e59-4693-8ad8-f0eaee32d1bf/5b9fdff5-be3d-4fc6-9606-dd1b4002f277/IN-en-20220530-popsignuptwoweeks-perspective_alpha_website_medium.jpg" alt="background-image" />
+      <div className="logo">
+      <img className='logo-image' src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png" alt="logo" />
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default Homepage
